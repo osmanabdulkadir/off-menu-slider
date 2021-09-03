920c0d473a884d10ca83d89c232aba47b11b31ea
osman abdul <74805866+osmanabdulkadir@users.noreply.github.com>
2021-09-03 16:51:43 -0400

    Add files via upload

---
+++ app.js
@@ -0,0 +1,9 @@
+let nav = document.getElementById("nav")
+let hamberger = document.getElementById("burger");
+
+hamberger.addEventListener('click', function() {
+
+   nav.classList.toggle("open")
+
+
+});
